859b9f7896a3f1d011a2275c2e82c78712554f3a
GUYARD Marc <marc@mguyard.com>
2024-05-09 17:59:05 +0200

    doc: resolve issue in python doc header

--- apps/CoversManager/covers_manager.py
+++ apps/CoversManager/covers_manager.py
@@ -8,7 +8,8 @@ from pydantic import ValidationError
 """
 Author : Marc Guyard
 Doc : https://github.com/mguyard/appdaemon-covermanager/blob/main/README.md
-Issue : 
+Bug Report : https://github.com/mguyard/appdaemon-covermanager/issues/new?assignees=&labels=bug&projects=&template=bug_report.yml
+Feature Request : https://github.com/mguyard/appdaemon-covermanager/issues/new?assignees=&labels=%F0%9F%9A%80%20feature-request&projects=&template=feature_request.yml
 
 """  # noqa: E501
 
